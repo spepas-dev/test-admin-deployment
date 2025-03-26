d383bd64f166ed78ce94675c605142c875ad2f35
Spa Pas <jboadi@spepas.com>
2025-03-26 17:10:39 +0100

    api call fixed

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import { ENV_CONFIG } from '@/config';
 
 export const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
